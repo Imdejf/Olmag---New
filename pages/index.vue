be1883efcdbe8da4fc41db4c90f5ec485034a3bf
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-05-30 10:48:48 +0200

    Fix top view for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -30,8 +30,9 @@ useHead({
   link: [
     {
       rel: "preload",
-      href: "assets/tlo_mniejsze.webp",
+      href: "/assets/tlo.webp",
       as: "image",
+      type: "image/webp",
     },
   ],
 });
@@ -51,23 +52,23 @@ useHead({
           <h1 class="font-base text-center text-md">
             Olmag.pl - wyposażenie magazynu
           </h1>
-          <div class="container mx-auto py-12 md:py-24">
+          <div class="container mx-auto py-4 md:py-24">
             <h2
-              class="text-2xl md:text-6xl text-center font-medium mb-4 capitalize"
+              class="text-lg md:text-6xl text-center font-bold md:font-medium mb-4 capitalize"
             >
               Sprawdź Naszą Ofertę Na Pojemniki Magazynowe I Drabiny Aluminiowe
             </h2>
-            <div class="mt-12 flex">
+            <div class="md:mt-12 flex">
               <div class="mx-auto md:flex text-center md:w-auto gap-10">
                 <NuxtLink
                   to="/category/pojemniki-magazynowe"
-                  class="bg-blue-900 block md:mt-0 hover:bg-opacity-80 text-orange-500 font-bold py-4 px-4 rounded-full"
+                  class="bg-blue-900 block md:mt-0 hover:bg-opacity-80 text-orange-500 font-semibold md:font-bold p-2.5 md:p-4 rounded-full"
                 >
                   POJEMNIKI MAGAZYNOWE
                 </NuxtLink>
                 <NuxtLink
                   to="/category/drabiny-aluminiowe"
-                  class="bg-blue-900 block mt-5 md:mt-0 hover:bg-opacity-80 text-orange-500 font-bold py-4 px-4 rounded-full"
+                  class="bg-blue-900 block mt-3 md:mt-0 hover:bg-opacity-80 text-orange-500 font-semibold md:font-bold p-2.5 md:p-4 rounded-full"
                 >
                   DRABINY ALUMINIOWE
                 </NuxtLink>

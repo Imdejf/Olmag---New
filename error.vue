dab397c2a2b3a05c2220493a20bf4ea05145368c
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-08 01:00:38 +0200

    Error page

--- error.vue
+++ error.vue
@@ -1,1 +1,13 @@
-<template>dsa</template>
+<script setup>
+const props = defineProps({
+  error: Object,
+  required: true,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <div>{{ error.statusCode }}</div>
+  <div><button @click="handleError">Go to the home page</button></div>
+</template>
